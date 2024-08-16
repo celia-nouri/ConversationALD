bb83c413cf396ff3bc501d769f072c5dbc37c150
Célia <celia.nouri@gmail.com>
2024-08-16 13:28:20 +0200

    temporal edges direction + start hetero graph

--- model_hateful_comments/models/model.py
+++ model_hateful_comments/models/model.py
@@ -1,10 +1,11 @@
 import torch
+import torch_geometric.transforms as T
 from torch_geometric.nn import RGCNConv, GraphConv, GATConv
-from torch_geometric.data import Data
+from torch_geometric.data import Data, HeteroData
 import torch.nn as nn
 import torch.nn.functional as F
 from transformers import AutoModel, DistilBertModel, RobertaModel, AutoModelForSequenceClassification
-from utils.construct_graph import get_graph
+from utils.construct_graph import get_graph, get_hetero_graph
 from torchsummary import summary
 
 from models.multimodal_transformer import GraphormerModel, GraphormerEncoder
@@ -241,7 +242,7 @@ class GATTest(torch.nn.Module):
     def forward(self, data):   
         # Save the original batch size
         #batch_size = x.size(0)
-        _, edges_dic_num = get_graph(data.x_text, with_temporal_edges=False, undirected=True)
+        _, edges_dic_num = get_graph(data.x_text, with_temporal_edges=False, undirected=False)
         assert len(edges_dic_num.keys()) <= 1, "length of edges dic num is greater than 1"
         edge_list = []
         device = get_device()
@@ -289,10 +290,126 @@ class GATTest(torch.nn.Module):
         out = self.fc(concat_out) 
         #out = self.text_pooler(out)
         #out = self.bert_dropout(out)
-        out = self.node_classifier(out)
-        #print('output of prediction: ', out)
-    
+        out = self.node_classifier(out)    
         return out
+
+
+class HeteroGAT(torch.nn.Module):
+    def __init__(self, in_channels=768, hidden_channels=768, num_heads_1=8, num_heads_2=1, dropout_1=0.6, dropout_2=0.6):
+        super(HeteroGAT, self).__init__()
+        #self.fc1 = nn.Linear(in_channels, hidden_channels)
+        self.gat1 = GATConv(in_channels, hidden_channels, heads=num_heads_1, dropout=dropout_1)
+        self.gat2 = GATConv(hidden_channels * num_heads_1, hidden_channels, heads=num_heads_2)
+        self.fc = torch.nn.Linear(1536, 768)  # Output one value for binary classification
+        bert = AutoModelForSequenceClassification.from_pretrained(
+            "bert-base-uncased",
+            hidden_dropout_prob=0.2,
+            attention_probs_dropout_prob=0.2,
+        )
+        self.text_model = bert.bert
+        self.text_pooler = self.text_model.pooler
+        self.node_classifier = bert.classifier
+        self.bert_dropout = bert.dropout
+    
+
+
+    def forward(self, data):   
+        # Save the original batch size
+        #batch_size = x.size(0)
+        num_comment_nodes, comments_edges_dic_num, num_users, user_to_comments_edges = get_hetero_graph(data.x_text, with_temporal_edges=True, undirected=False)
+
+
+        hetero_graph = HeteroData()
+
+        # Add node indices:
+        hetero_graph["comments"].node_id = torch.arange(num_comment_nodes)
+        hetero_graph["users"].node_id = torch.arange(num_users)
+
+        # Add edge indices:
+        hetero_graph["users", "post", "comments"].edge_index = user_to_comments_edges
+        hetero_graph["comments", "reply", "comments"].edge_index = comments_edge_list
+
+        # We also need to make sure to add the reverse edges from movies to users
+        # in order to let a GNN be able to pass messages in both directions.
+        # We can leverage the `T.ToUndirected()` transform for this from PyG:
+        if undirected:
+            hetero_graph = T.ToUndirected()(hetero_graph)
+
+        x_token_type_ids = x["token_type_ids"] # [#comments, 100]
+        x_attention_mask = x["attention_mask"] # [#comments, 100]
+        x_input_ids = x["input_ids"] # [#comments, 100]
+        bert_output = self.text_model(
+            token_type_ids=x_token_type_ids,
+            attention_mask=x_attention_mask,
+            input_ids=x_input_ids,
+        ).last_hidden_state # [#comments, 100, 768]
+        # Add node features
+        hetero_graph["comments"].x = bert_output
+
+        print(hetero_graph)
+
+
+        
+        g_data = Data(x=bert_output, edge_index=edge_list.t().contiguous())
+
+
+        assert len(edges_dic_num.keys()) <= 1, "length of edges dic num is greater than 1"
+        edge_list = []
+        device = get_device()
+        for k in edges_dic_num.keys():
+            edge_list = edges_dic_num[k]
+            edge_list = sorted(edge_list, key=lambda x: (x[0], x[1]))
+            edge_list = torch.tensor(edge_list).to(device)
+        x = data.x
+        #edge_indices = data.edge_index.permute(1, 0)
+        mask = data["y_mask"]
+        #are_equal = torch.equal(edge_list, edge_indices)
+        #print(f"Are the tensors equal? {are_equal}")
+        # YES. The undirected option without temporal edges is equal to edge_indices (the edge list returned by the mDT codebase.)
+
+        # x["token_type_ids"] is [#comments, 100]
+        x_token_type_ids = x["token_type_ids"] # [#comments, 100]
+        x_attention_mask = x["attention_mask"] # [#comments, 100]
+        x_input_ids = x["input_ids"] # [#comments, 100]
+        bert_output = self.text_model(
+            token_type_ids=x_token_type_ids,
+            attention_mask=x_attention_mask,
+            input_ids=x_input_ids,
+        ).last_hidden_state # [#comments, 100, 768]
+
+        
+        g_data = Data(x=bert_output, edge_index=edge_list.t().contiguous())
+        x, edge_list = g_data.x, g_data.edge_index
+        x = x.to(device)
+        edge_indices = edge_list.to(device)
+        cls_embeddings = bert_output[:, 0, :] 
+         # GAT layer 1
+        x = self.gat1(cls_embeddings, edge_indices)
+        x = F.elu(x)
+         # GAT layer 2
+        x = self.gat2(x, edge_indices)
+        x_gemb = x[mask, :]
+        x_emb = cls_embeddings[mask, :]
+
+        concat_out = torch.cat([x_gemb, x_emb], dim=1)
+
+        # SHAPE OF X: [#vertices, 768=input_dim] --> [#vertices, 64=hidden_dim] --> [#vertices, 1] --> [#vertices]
+        
+
+        # Classification layer (binary classification)
+        out = self.fc(concat_out) 
+        #out = self.text_pooler(out)
+        #out = self.bert_dropout(out)
+        out = self.node_classifier(out)    
+        return out
+
+
+
+
+
+get_hetero_graph
+
+
 def get_model(args, model_name, hidden_channels=64, num_heads=1):
     assert model_name in all_model_names, "Invalid model name: {}".format(model_name)
     device = get_device()
